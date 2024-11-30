f486f3d92332ba8872e519ab54e20f636b806b77
Thomas Bono <thomasbono.com@gmail.com>
2024-11-29 23:22:45 -0500

    dark mode working now

--- app.vue
+++ app.vue
@@ -1,11 +1,52 @@
 <script setup>
+const colorMode = useColorMode()
 function toggleDarkMode() {
-  document.documentElement.classList.toggle("dark")
+  document.documentElement.classList.toggle("dark-mode")
 }
 </script>
 <template>
   <div>
     <h1>Hello world!</h1>
+
+    <div>
+      <p class="p-4 pb-2">
+        <select
+          v-model="colorMode.preference"
+          class="border w-24 h-8 dark:bg-gray-900 dark:text-white dark:border-gray-700"
+        >
+          <option value="system">System</option>
+          <option value="light">Light</option>
+          <option value="dark">Dark</option>
+        </select>
+      </p>
+      <div class="p-4">
+        <div
+          class="max-w-sm rounded overflow-hidden shadow-lg mb-4 bg-white dark:bg-gray-900"
+        >
+          <img
+            class="w-full"
+            src="https://tailwindcss.com/img/card-top.jpg"
+            alt="Sunset in the mountains"
+          />
+          <div class="px-6 py-4 pb-2">
+            <div class="font-bold text-xl mb-2 dark:text-white">The Coldest Sunset</div>
+            <p class="text-gray-700 dark:text-gray-300">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia,
+              nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div class="px-6 py-4">
+            <span class="badge mr-2">#photography</span>
+            <span class="badge mr-2">#travel</span>
+            <span class="badge">#winter</span>
+          </div>
+          <div class="px-6 py-4 dark-mode">
+            <span class="badge mr-2">#staysdark</span>
+            <span class="badge force-light">#stayslight</span>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <Button severity="success" label="Toggle Dark Mode" @click="toggleDarkMode()" />
     <Button label="Toggle Dark Mode" @click="toggleDarkMode()" />
@@ -23,6 +64,6 @@ h1 {
   @apply text-3xl font-bold underline;
 }
 .box {
-  @apply p-2 bg-red-700 [.dark_&]:bg-red-300 rounded-lg w-20 text-center text-p-primary-100;
+  @apply p-2 bg-red-700 dark:bg-red-300 rounded-lg w-20 text-center text-p-primary-100;
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import Aura from '@primevue/themes/aura';
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss'],
+  modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
   css: ['~/src/assets/main.css'],
   // postcss: {
   //   plugins: {
